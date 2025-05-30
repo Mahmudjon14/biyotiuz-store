d22be0c7027020a9fa32413ddbaabc2702ebe3ba
Mahmudjon14 <jorayevmahmudjon8@gmail.com>
2025-05-30 11:17:59 +0500

    Update admin.py

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,1 +1,21 @@
-# Temporary file
+from fastapi import APIRouter
+from app.orders import orders_db
+from collections import Counter
+
+router = APIRouter()
+
+@router.get("/stats")
+def get_stats():
+    total_orders = len(orders_db)
+    item_counter = Counter()
+
+    for order in orders_db:
+        for item in order["items"]:
+            item_counter[item["name"]] += item["quantity"]
+
+    top_selling = item_counter.most_common(5)
+
+    return {
+        "total_orders": total_orders,
+        "top_selling": top_selling
+    }
