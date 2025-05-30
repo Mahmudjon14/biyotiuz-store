3e8b5b976fdddfc7a9bed525a70320c977a0b4e3
Mahmudjon14 <jorayevmahmudjon8@gmail.com>
2025-05-30 12:55:39 +0500

    Update orders.py

--- backend/app/orders.py
+++ backend/app/orders.py
@@ -1,17 +1,8 @@
-from fastapi import APIRouter, Request
-from app.database import orders, generate_id
+from fastapi import APIRouter
+from backend.app.database import orders_db  # To‘g‘rilangan import
 
 router = APIRouter()
 
-@router.post("/")
-async def create_order(order_data: dict, request: Request):
-    order_id = generate_id()
-    order_data["id"] = order_id
-    order_data["ip"] = request.client.host
-    order_data["status"] = "New"
-    orders.append(order_data)
-    return {"message": "Buyurtma qabul qilindi", "order_id": order_id}
-
 @router.get("/")
-async def get_orders():
-    return orders
+async def get_all_orders():
+    return orders_db
